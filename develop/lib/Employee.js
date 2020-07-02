c57b6d08b995a31aad5070145f88dcd4361acbd7
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-02 18:52:24 -0400

    updated js files and saving

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,4 +1,7 @@
 // TODO: Write code to define and export the Employee class
+
+const Employee = require("./Employee");
+
 class Employee {
     constructor (name, role, id) {
         this.name = name;
