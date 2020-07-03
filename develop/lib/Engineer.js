988bdd5cb50976b2b9b7fcf2a33a976761280946
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-02 22:02:34 -0400

    progress on testing. finished employee testing and am starting engineer testing

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,11 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
-const employee = require("./Employee");
+const Employee = require("./Employee");
 const engineer = require("./Engineer");
 
 class Engineer extends Employee {
     constructor (name,roll,id,github,email){
         super(name,roll,id,email);
         this.github = github;
+    }
+    github(){
+        return this.github;
     }
 };
